54068644d85e8cdfc97b6274b1600f6cd818bc56
ricmaps <ricardomapurungajunior@gmail.com>
2026-03-10 16:11:43 -0300

    create flake.nix

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Halloy Community Themes packaged for nix consumption";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      flake = {
+        overlays.halloy-theme = final: prev: {
+          halloy-theme = self.packages.${prev.stdenv.hostPlatform.system};
+        };
+        overlays.default = self.overlays.halloy-theme;
+      };
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { lib, pkgs, ... }:
+        let
+          withoutTOMLExtension = file: lib.removeSuffix ".toml" file;
+          themeFiles =
+            lib.attrNames (builtins.readDir ./themes);
+          mkThemePackage = themeFile:
+            let name = withoutTOMLExtension themeFile;
+            in lib.nameValuePair name (pkgs.stdenvNoCC.mkDerivation {
+              inherit name;
+              dontUnpack = true;
+              dontConfigure = true;
+              dontBuild = true;
+              installPhase = ''
+                runHook preInstall
+                cp --reflink=auto ./themes/${themeFile} $out
+                runHook postInstall
+              '';
+            });
+          themeDerivations = map mkThemePackage themeFiles;
+        in { packages = lib.listToAttrs themeDerivations; };
+    };
+}
